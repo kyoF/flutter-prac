059ccd7d01b4e7acfa456f26857c1047ecb3e305
kyoF <fujiki.kyosuke@gmail.com>
2022-04-02 11:50:24 +0900

    テストファイルのコメントを編集

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,3 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -12,19 +5,22 @@ import 'package:hello_world_from_flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+    // 画面を構築する
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
+    // 0が書かれているWidgetが1つであることをテスト
     expect(find.text('0'), findsOneWidget);
+    // 1が書かれているWidgetがないことをテスト
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
+    // アイコンが+のWidgetをタップ
     await tester.tap(find.byIcon(Icons.add));
+    // Widgetツリーの再構築
     await tester.pump();
 
-    // Verify that our counter has incremented.
+    // 0が書かれているWidgetがないことをテスト
     expect(find.text('0'), findsNothing);
+    // 1が書かれているWidgetが1つであることをテスト
     expect(find.text('1'), findsOneWidget);
   });
 }
