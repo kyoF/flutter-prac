f08af27c5838b3004a260022f24b0e3697bf623c
kyoF <fujiki.kyosuke@gmail.com>
2022-04-07 19:33:33 +0900

    メモ帳アプリを作ってみる

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:hello_world_from_flutter/edit.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(new MyApp());
@@ -7,36 +10,140 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
+    return MaterialApp(
+      title: 'Memo App',
+      theme: ThemeData(
         primaryColor: Colors.white,
       ),
-      home: new MyHomePage(),
+      home: MemoList(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MemoList extends StatefulWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  MemoListState createState() => MemoListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class MemoListState extends State<MemoList> {
+  List<String> _memoList = [];
+  var _currentIndex = -1;
+  bool _loading = true;
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  void initState() {
+    super.initState();
+    this.loadMemoList();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('HOME'),
+    final title = 'Home';
+    if (_loading) {
+      return Scaffold(
+          appBar: AppBar(
+            title: Text(title),
+          ),
+          body: CircularProgressIndicator());
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
       ),
-      body: ListView(
-        children: List.generate(20, (int index) {
-          return Card(
-            child: ListTile(
-                title: Text('test$index'), subtitle: Text('sub title')),
-          );
-        }),
+      body: _buildList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addMemo,
+        tooltip: 'New Memo',
+        child: Icon(Icons.add),
       ),
+    );
+  }
+
+  void loadMemoList() {
+    SharedPreferences.getInstance().then((prefs) {
+      const key = 'memo-list';
+      if (prefs.containsKey(key)) {
+        _memoList = prefs.getStringList(key)!;
+      }
+      setState(() {
+        _loading = false;
+      });
+    });
+  }
+
+  void _addMemo() {
+    setState(() {
+      _memoList.add("");
+      _currentIndex = _memoList.length - 1;
+      storeMemoList();
+      Navigator.of(context).push(MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          return new Edit(_memoList[_currentIndex], _onChanged);
+        },
+      ));
+    });
+  }
+
+  void _onChanged(String text) {
+    setState(() {
+      _memoList[_currentIndex] = text;
+      storeMemoList();
+    });
+  }
+
+  void storeMemoList() async {
+    final prefs = await SharedPreferences.getInstance();
+    const key = 'memo-list';
+    final success = await prefs.setStringList(key, _memoList);
+    if (!success) {
+      debugPrint('Failed to store value');
+    }
+  }
+
+  Widget _buildList() {
+    final itemCount = _memoList.length == 0 ? 0 : _memoList.length * 2 - 1;
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemCount: itemCount,
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider(height: 2);
+          final index = (i / 2).floor();
+          final memo = _memoList[index];
+          return _buildWrappedRow(memo, index);
+        });
+  }
+
+  Widget _buildWrappedRow(String content, int index) {
+    return Dismissible(
+      background: Container(color: Colors.red),
+      key: Key(content),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        setState(() {
+          _memoList.removeAt(index);
+          storeMemoList();
+        });
+      },
+      child: _buildRow(content, index),
+    );
+  }
+
+  Widget _buildRow(String content, int index) {
+    return ListTile(
+      title: Text(
+        content,
+        style: _biggerFont,
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+      ),
+      onTap: () {
+        _currentIndex = index;
+        Navigator.of(context)
+            .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+          return new Edit(_memoList[_currentIndex], _onChanged);
+        }));
+      },
     );
   }
 }
