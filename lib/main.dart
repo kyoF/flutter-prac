427faa8267b46e10a3a4b5e119b2b27c752814aa
kyoF <fujiki.kyosuke@gmail.com>
2022-04-07 17:29:30 +0900

    メモのリスト表示をしてみる

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world_from_flutter/async.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return new MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: new ThemeData(
+        primaryColor: Colors.white,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
-  final String? title;
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-    Async().asynctest1();
-  }
-
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title!),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('HOME'),
       ),
-      body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('You have pushed the button this many times:'),
-              Text('$_counter',
-                  key: Key('counter'),
-                  style: Theme.of(context).textTheme.headline4)
-            ]),
+      body: ListView(
+        children: List.generate(20, (int index) {
+          return Card(
+            child: ListTile(
+                title: Text('test$index'), subtitle: Text('sub title')),
+          );
+        }),
       ),
-      floatingActionButton: FloatingActionButton(
-          key: Key('increment'),
-          onPressed: _incrementCounter,
-          tooltip: 'Increment',
-          child: Icon(Icons.add)),
     );
   }
 }
