50ac58418ff80681d11d389e9614680e7e2a6ccf
kyoF <fujiki.kyosuke@gmail.com>
2022-04-13 11:29:08 +0900

    counterの偶奇判定を自力で実装してみた

--- lib/main.dart
+++ lib/main.dart
@@ -1,149 +1,74 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:hello_world_from_flutter/edit.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(new MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Memo App',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primaryColor: Colors.white,
+        primarySwatch: Colors.blue,
       ),
-      home: MemoList(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MemoList extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
   @override
-  MemoListState createState() => MemoListState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class MemoListState extends State<MemoList> {
-  List<String> _memoList = [];
-  var _currentIndex = -1;
-  bool _loading = true;
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  String _type = 'even';
 
-  @override
-  void initState() {
-    super.initState();
-    this.loadMemoList();
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      if (_counter % 2 == 0) {
+        _type = 'even';
+      } else {
+        _type = 'odd';
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final title = 'Home';
-    if (_loading) {
-      return Scaffold(
-          appBar: AppBar(
-            title: Text(title),
-          ),
-          body: CircularProgressIndicator());
-    }
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
-      body: _buildList(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Text('$_type', style: TextStyle(fontSize: 20, color: Colors.red)),
+            if (_type == 'even') Text('even no odd kan'),
+            if (_type == 'odd') Text('sonnna koto naiyo'),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _addMemo,
-        tooltip: 'New Memo',
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
-    );
-  }
-
-  void loadMemoList() {
-    SharedPreferences.getInstance().then((prefs) {
-      const key = 'memo-list';
-      if (prefs.containsKey(key)) {
-        _memoList = prefs.getStringList(key)!;
-      }
-      setState(() {
-        _loading = false;
-      });
-    });
-  }
-
-  void _addMemo() {
-    setState(() {
-      _memoList.add("");
-      _currentIndex = _memoList.length - 1;
-      storeMemoList();
-      Navigator.of(context).push(MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          return new Edit(_memoList[_currentIndex], _onChanged);
-        },
-      ));
-    });
-  }
-
-  void _onChanged(String text) {
-    setState(() {
-      _memoList[_currentIndex] = text;
-      storeMemoList();
-    });
-  }
-
-  void storeMemoList() async {
-    final prefs = await SharedPreferences.getInstance();
-    const key = 'memo-list';
-    final success = await prefs.setStringList(key, _memoList);
-    if (!success) {
-      debugPrint('Failed to store value');
-    }
-  }
-
-  Widget _buildList() {
-    final itemCount = _memoList.length == 0 ? 0 : _memoList.length * 2 - 1;
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemCount: itemCount,
-        itemBuilder: (context, i) {
-          if (i.isOdd) return Divider(height: 2);
-          final index = (i / 2).floor();
-          final memo = _memoList[index];
-          return _buildWrappedRow(memo, index);
-        });
-  }
-
-  Widget _buildWrappedRow(String content, int index) {
-    return Dismissible(
-      background: Container(color: Colors.red),
-      key: Key(content),
-      direction: DismissDirection.endToStart,
-      onDismissed: (direction) {
-        setState(() {
-          _memoList.removeAt(index);
-          storeMemoList();
-        });
-      },
-      child: _buildRow(content, index),
-    );
-  }
-
-  Widget _buildRow(String content, int index) {
-    return ListTile(
-      title: Text(
-        content,
-        style: _biggerFont,
-        maxLines: 1,
-        overflow: TextOverflow.ellipsis,
-      ),
-      onTap: () {
-        _currentIndex = index;
-        Navigator.of(context)
-            .push(MaterialPageRoute<void>(builder: (BuildContext context) {
-          return new Edit(_memoList[_currentIndex], _onChanged);
-        }));
-      },
     );
   }
 }
